ca20e84518c3c30b4d8ce5e0834e8b022c6cf89d
Daniil <dvinakovich@gmail.com>
2024-02-03 00:01:22 +0300

    Created insight section and added swiper

---
+++ src/js/components/slider.js
@@ -0,0 +1,16 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+export const useInsightSlider = () => {
+  new Swiper('.insight__slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import '../assets/scss/main.scss';
 // component
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { useInsightSlider } from './components/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();

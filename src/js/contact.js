2aad28d95821c4247a122169e8b2a308fde2ff17
Daniil <dvinakovich@gmail.com>
2024-02-08 01:03:01 +0300

    Created contact:hero section and added contact:partners section

---
+++ src/js/contact.js
@@ -0,0 +1,8 @@
+import '../assets/scss/contact.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+
+useTheme();
+useBurger();

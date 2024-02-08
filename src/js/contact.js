4ee70d8e968f1794f2e51801963e8aa8d8aee269
Daniil <dvinakovich@gmail.com>
2024-02-08 04:00:35 +0300

    Created contact:get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,6 +3,8 @@ import '../assets/scss/contact.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js';
 
 useTheme();
 useBurger();
+usePhone();
